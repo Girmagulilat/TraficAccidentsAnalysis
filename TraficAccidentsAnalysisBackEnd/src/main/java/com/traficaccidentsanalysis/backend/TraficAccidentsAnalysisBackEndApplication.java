f7cb4c1bf4031c8a0569de161b9468e02cc6123b
biruckf <49043559+biruckf@users.noreply.github.com>
2020-03-01 23:19:28 -0500

    initialcommit

---
+++ TraficAccidentsAnalysisBackEnd/src/main/java/com/traficaccidentsanalysis/backend/TraficAccidentsAnalysisBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.traficaccidentsanalysis.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraficAccidentsAnalysisBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraficAccidentsAnalysisBackEndApplication.class, args);
+	}
+
+}
---
+++ TraficAccidentsAnalysisBackEnd/src/test/java/com/traficaccidentsanalysis/backend/TraficAccidentsAnalysisBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.traficaccidentsanalysis.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraficAccidentsAnalysisBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
